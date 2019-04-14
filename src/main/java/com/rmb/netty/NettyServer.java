588cae3e1ade4fd4b4e7cd4cbd5570ba12846d53
wangkai <wangkai112138@163.com>
2019-04-14 19:02:52 +0800

    first commit

---
+++ src/main/java/com/rmb/netty/NettyServer.java
@@ -0,0 +1,42 @@
+package com.rmb.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Created by wk on 2019/4/14.
+ */
+public class NettyServer {
+
+    public static void main(String[] args) {
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        serverBootstrap
+                .group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                })
+                .bind(8000);
+
+    }
+
+}
